33d8e422156517a8657460481b1fb4d4a125cf93
Paul Cuzner <pcuzner@redhat.com>
2018-11-29 09:30:40 +1300

    Give the Next button a default
    Enable the next button to accept a different text
    label
    
    Signed-off-by: Paul Cuzner <pcuzner@redhat.com>

--- src/components/common/nextbutton.jsx
+++ src/components/common/nextbutton.jsx
@@ -10,9 +10,15 @@ export class NextButton extends React.Component {
         } else {
             btnState = this.props.disabled;
         }
+        var btnText;
+        if (this.props.btnText != undefined) {
+            btnText = this.props.btnText;
+        } else {
+            btnText = 'Next';
+        }
 
         return (
-            <button className="bottomRight btn btn-primary btn-lg" disabled={btnState} onClick={this.props.action}>Next</button>
+            <button className="bottomRight btn btn-primary btn-lg" disabled={btnState} onClick={this.props.action}>{btnText}</button>
         );
     }
 }
