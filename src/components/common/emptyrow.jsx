07774d1e93f192345ecbded78d65a9aad33bb479
Paul Cuzner <pcuzner@redhat.com>
2018-12-20 17:48:33 +1300

    Wrap empty row in a tbody segment
    tbody is used as a means of grouping rows
    
    Signed-off-by: Paul Cuzner <pcuzner@redhat.com>

--- src/components/common/emptyrow.jsx
+++ src/components/common/emptyrow.jsx
@@ -3,8 +3,10 @@ import '../../app.scss';
 
 export const emptyRow = () => {
     return (
-        <tr className="emptyTable">
-            <td colSpan="9" className="emptyTable">No Hosts Defined</td>
-        </tr>
+        <tbody>
+            <tr className="emptyTable">
+                <td colSpan="9" className="emptyTable">No Hosts Defined</td>
+            </tr>
+        </tbody>
     );
 };
