346d6a039c92a73a066a6b60b1344b294d89f380
Paul Cuzner <pcuzner@redhat.com>
2018-12-21 18:27:00 +1300

    Add validation summary table to Review page
    Signed-off-by: Paul Cuzner <pcuzner@redhat.com>

--- src/components/reviewpage.jsx
+++ src/components/reviewpage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NextButton } from './common/nextbutton.jsx';
-import { buildRoles, hostsWithRoleCount } from '../services/utils.js';
+import { buildRoles, hostsWithRoleCount, msgCount } from '../services/utils.js';
 import '../app.scss';
 
 export class ReviewPage extends React.Component {
@@ -22,6 +22,13 @@ export class ReviewPage extends React.Component {
 
         this.environmentData = {};
         this.clusterData = {};
+        this.validationData = {
+            Critical: 0,
+            Error: 0,
+            Warning: 0,
+            Info: 0
+        };
+        this.hostList = [];
     }
 
     componentWillReceiveProps (props) {
@@ -49,6 +56,7 @@ export class ReviewPage extends React.Component {
                 this.clusterData["- " + role] = hostsWithRoleCount(props.config.hosts, roleName);
             }
             let osdCount = 0;
+
             for (let host of props.config.hosts) {
                 switch (props.config.flashUsage) {
                 case "OSD Data":
@@ -57,6 +65,17 @@ export class ReviewPage extends React.Component {
                 default:
                     osdCount += host.hdd;
                 }
+                let msgStats = msgCount(host.msgs);
+                for (let mType of Object.keys(msgStats)) {
+                    let mTypeKey = mType.charAt(0).toUpperCase() + mType.slice(1);
+                    // console.log(msgType);
+                    if (this.validationData.hasOwnProperty(mTypeKey)) {
+                        this.validationData[mTypeKey] += msgStats[mType];
+                    } else {
+                        this.validationData[mTypeKey] = msgStats[mType];
+                    }
+                }
+                console.log(JSON.stringify(msgStats));
             }
             this.clusterData['OSD devices'] = osdCount;
             this.clusterData['Public Network'] = props.config.publicNetwork;
@@ -64,12 +83,13 @@ export class ReviewPage extends React.Component {
             if (props.config.rgwNetwork) {
                 this.clusterData['S3 Network'] = props.config.rgwNetwork;
             }
+
+            this.hostList = JSON.parse(JSON.stringify(this.props.config.hosts));
         }
     }
 
     render() {
         console.log("in review render");
-        // let environment = this.environmentData();
         return (
 
             <div id="review" className={this.props.className}>
@@ -79,7 +99,10 @@ export class ReviewPage extends React.Component {
                     <StaticTable title="Environment" data={this.environmentData} align="left" />
                     <div className="review-table-whitespace" />
                     <StaticTable title="Cluster" data={this.clusterData} align="right" />
+                    <div className="review-table-whitespace" />
+                    <StaticTable title="Validation Summary" data={this.validationData} align="right" />
                 </div>
+                <HostListing hosts={this.hostList} />
                 <NextButton action={this.props.action} />
             </div>
         );
@@ -114,4 +137,13 @@ class StaticTable extends React.Component {
         );
     }
 }
+
+class HostListing extends React.Component {
+    render () {
+        return (
+            <div />
+        );
+    }
+}
+
 export default ReviewPage;
