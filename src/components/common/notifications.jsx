c81d0a28f94069c33c95349dd849a23e2ea23cb2
Paul Cuzner <pcuzner@redhat.com>
2019-01-01 19:38:52 +1300

    Add Inline-notification component
    Signed-off-by: Paul Cuzner <pcuzner@redhat.com>

---
+++ src/components/common/notifications.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import '../../app.scss';
+
+export class Notification extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    // msgLevel = error, warning, info
+    // msgText = text string / jsx component
+
+    forceUpdateHandler = () => {
+        this.forceUpdate();
+    }
+
+    render() {
+        let msgClass;
+        let msgIcon;
+        let notification;
+        switch (this.props.msgLevel) {
+        case "error":
+            msgClass = "alert alert-danger";
+            msgIcon = "pficon pficon-error-circle-o";
+            break;
+        case "warning":
+            msgClass = "alert alert-warning";
+            msgIcon = "pficon pficon-warning-triangle-o";
+            break;
+        case "success":
+            msgClass = "alert alert-success";
+            msgIcon = "pficon pficon-ok";
+            break;
+        default:
+            msgClass = "alert alert-info";
+            msgIcon = "pficon pficon-info";
+            break;
+        }
+        // message = (this.props.msgText) ? this.props.msgText : 'dummy message';
+        if (this.props.msgText) {
+            notification = (
+                <div className={msgClass} >
+                    <span className={msgIcon} />
+                    { this.props.msgText }
+                </div>
+            );
+        } else {
+            notification = (
+                <div className="alert-space" />
+            );
+        }
+        return (
+            <div>{ notification }</div>
+        );
+    }
+}
