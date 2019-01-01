00ac611b14ea6a05f3079b54fab9521ed1439c88
Paul Cuzner <pcuzner@redhat.com>
2019-01-01 19:44:35 +1300

    Progress tracker navigation removed
    Signed-off-by: Paul Cuzner <pcuzner@redhat.com>

--- src/components/progresstracker.jsx
+++ src/components/progresstracker.jsx
@@ -12,29 +12,42 @@ export class ProgressTracker extends React.Component {
         if (this.props.pageNum >= 1) {
             for (let i = 1; i < 7; i++) {
                 if (i == this.props.pageNum) {
-                    this.itemState[i] = "indicator selected selectable";
+                    this.itemState[i] = "indicator selected";
                 } else {
-                    let current = this.itemState[i];
-                    current = current.replace('selected', '');
-                    this.itemState[i] = current;
+                    // let current = this.itemState[i];
+                    // current = current.replace('selected', '');
+                    this.itemState[i] = "indicator";
                 }
             }
         }
 
         return (
             <div id="navcontainer">
-                <span label-name="Environment" onClick={ () => this.props.pageSwitcher(1) } className={this.itemState[1]}>1</span>
+                <span label-name="Environment" className={this.itemState[1]}>1</span>
                 <span className="joiner">&nbsp;</span>
-                <span label-name="Hosts" onClick={() => this.props.pageSwitcher(2)} className={this.itemState[2]}>2</span>
+                <span label-name="Hosts" className={this.itemState[2]}>2</span>
                 <span className="joiner">&nbsp;</span>
-                <span label-name="Validate" onClick={() => this.props.pageSwitcher(3)} className={this.itemState[3]}>3</span>
+                <span label-name="Validate" className={this.itemState[3]}>3</span>
                 <span className="joiner">&nbsp;</span>
-                <span label-name="Network" onClick={() => this.props.pageSwitcher(4)} className={this.itemState[4]}>4</span>
+                <span label-name="Network" className={this.itemState[4]}>4</span>
                 <span className="joiner">&nbsp;</span>
-                <span label-name="Review" onClick={() => this.props.pageSwitcher(5)} className={this.itemState[5]}>5</span>
+                <span label-name="Review" className={this.itemState[5]}>5</span>
                 <span className="joiner">&nbsp;</span>
-                <span label-name="Deploy" onClick={() => this.props.pageSwitcher(6)} className={this.itemState[6]}>6</span>
+                <span label-name="Deploy" className={this.itemState[6]}>6</span>
             </div>
+            // <div id="navcontainer">
+            //     <span label-name="Environment" onClick={ () => this.props.pageSwitcher(1) } className={this.itemState[1]}>1</span>
+            //     <span className="joiner">&nbsp;</span>
+            //     <span label-name="Hosts" onClick={() => this.props.pageSwitcher(2)} className={this.itemState[2]}>2</span>
+            //     <span className="joiner">&nbsp;</span>
+            //     <span label-name="Validate" onClick={() => this.props.pageSwitcher(3)} className={this.itemState[3]}>3</span>
+            //     <span className="joiner">&nbsp;</span>
+            //     <span label-name="Network" onClick={() => this.props.pageSwitcher(4)} className={this.itemState[4]}>4</span>
+            //     <span className="joiner">&nbsp;</span>
+            //     <span label-name="Review" onClick={() => this.props.pageSwitcher(5)} className={this.itemState[5]}>5</span>
+            //     <span className="joiner">&nbsp;</span>
+            //     <span label-name="Deploy" onClick={() => this.props.pageSwitcher(6)} className={this.itemState[6]}>6</span>
+            // </div>
         );
     }
 }
