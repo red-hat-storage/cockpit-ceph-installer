8e04e4a2f3a1c49a21509eb91e0cea67407f7e3c
Paul Cuzner <pcuzner@redhat.com>
2019-05-03 10:38:52 +1200

    Apply defaults received from parent
    Defaults received are used when rendering this
    page's components. In addition function binding
    has been replaced by arrow functions for
    consistency
    
    Signed-off-by: Paul Cuzner <pcuzner@redhat.com>

--- src/components/environmentpage.jsx
+++ src/components/environmentpage.jsx
@@ -6,20 +6,23 @@ import { Selector } from './common/selector.jsx';
 import '../app.scss';
 
 export class EnvironmentPage extends React.Component {
+    //
+    // this page offers the high level cluster configuration options like
+    // version/release and filestore/bluestore. Defaults are defined in
+    // the components initial state
+
     constructor(props) {
         super(props);
-        this.updateState = this.updateState.bind(this);
-        this.checkReady = this.checkReady.bind(this);
         this.state = {
             className: this.props.className,
-            osdType: "Bluestore",
-            networkType: "ipv4",
-            sourceType: "Red Hat",
-            clusterType: "Production",
-            osdMode: "Standard",
-            installType: "Container",
-            flashUsage: "Journals/Logs",
-            targetVersion: "RHCS 3"
+            osdType: props.defaults.osdType,
+            networkType: props.defaults.networkType,
+            sourceType: props.defaults.sourceType,
+            clusterType: props.defaults.clusterType,
+            osdMode: props.defaults.osdMode,
+            installType: props.defaults.installType,
+            flashUsage: props.defaults.flashUsage,
+            targetVersion: props.defaults.targetVersion
         };
 
         this.installSource = {
@@ -33,13 +36,9 @@ export class EnvironmentPage extends React.Component {
             tooltip : "Production mode applies strict configuration rules. To relax rules for\na developer or POC, use Development/POC mode"
         };
 
-        // TODO: These settings should come from the parent, which in turn should be
-        // the result of a cockpit.file read request, so the config is editable from
-        // a file - instead of hacking code!
         this.osd_type = {
             description: "OSD type",
             options: ["Bluestore", "Filestore"],
-            default: "Bluestore",
             name: "osdType",
             tooltip: "Bluestore is the default OSD type, offering more features and improved\nperformance. Filestore is supported as a legacy option only",
             horizontal: true
@@ -47,7 +46,6 @@ export class EnvironmentPage extends React.Component {
         this.network_type = {
             description: "Network connectivity",
             options: ['ipv4'], // 'ipv6'],
-            default: 'ipv4',
             name: 'networkType',
             tooltip: "",
             horizontal: true
@@ -55,7 +53,6 @@ export class EnvironmentPage extends React.Component {
         this.source = {
             description: "Software source",
             options: ["Red Hat", "OS Distribution", "Community"],
-            default: "Red Hat",
             name: "sourceType",
             tooltip: '',
             horizontal: true
@@ -63,7 +60,6 @@ export class EnvironmentPage extends React.Component {
         this.osd_mode = {
             description: "Encryption",
             options: ["None", "Encrypted"],
-            default: "None",
             name: "osdMode",
             info: "For added security, you may use at-rest encryption for your storage devices",
             tooltip: "Data encryption uses the Linux dmcrypt subsystem (LUKS1)",
@@ -73,7 +69,6 @@ export class EnvironmentPage extends React.Component {
         this.install_type = {
             description: "Installation type",
             options: ["Container", "RPM"],
-            default: "Container",
             name: "installType",
             info: "Ceph can be installed as lightweight container images, or as rpm packages. Container deployments offer service isolation enabling improved collocation and hardware utilization",
             tooltip: "Containers simplify deployment",
@@ -82,7 +77,6 @@ export class EnvironmentPage extends React.Component {
         this.flash_usage = {
             description: "Flash Configuration",
             options: ["Journals/Logs", "OSD Data"],
-            default: "Journals/Logs",
             name: "flashUsage",
             info: "Flash media (SSD or NVMe) can be used for all data, or as journal devices to improve the performance of slower devices (HDDs)",
             tooltip: "In Journal 'mode', the installation process will check HDD:Flash media\nratios against best practice",
@@ -108,12 +102,12 @@ export class EnvironmentPage extends React.Component {
         console.log("changing version : " + event.target.value);
     }
 
-    updateState(event) {
+    updateState = (event) => {
         console.log("received a state change for radio button: " + event.target.getAttribute('name') + " with " + event.target.value);
         this.setState({ [event.target.getAttribute('name')]: event.target.value });
     }
 
-    checkReady(event) {
+    checkReady = (event) => {
         // insert any validation logic here - that would compare the state settings prior to passing page state to the parent
         console.log("current radio button config: " + JSON.stringify(this.state));
         this.props.action(this.state);
@@ -127,15 +121,15 @@ export class EnvironmentPage extends React.Component {
                 <h3>1. Environment</h3>
                 Define the high level environment settings that will determine the way that the Ceph cluster is installed and configured.
                 <div >
-                    <Selector labelName="Installation Source" vertical options={Object.keys(this.installSource)} callback={this.installChange} />
+                    <Selector labelName="Installation Source" vertical value={this.state.installSource} options={Object.keys(this.installSource)} callback={this.installChange} />
                     <Selector labelName="Target Version" vertical value={this.state.targetVersion} options={versionList} callback={this.versionChange} />
-                    <Selector labelName="Cluster Type" vertical options={this.clusterTypes.options} tooltip={this.clusterTypes.tooltip} callback={this.clusterTypeChange} />
+                    <Selector labelName="Cluster Type" vertical value={this.state.clusterType} options={this.clusterTypes.options} tooltip={this.clusterTypes.tooltip} callback={this.clusterTypeChange} />
                 </div>
-                <RadioSet config={this.network_type} callback={this.updateState} />
-                <RadioSet config={this.osd_type} callback={this.updateState} />
-                <RadioSet config={this.flash_usage} callback={this.updateState} />
-                <RadioSet config={this.osd_mode} callback={this.updateState} />
-                <RadioSet config={this.install_type} callback={this.updateState} />
+                <RadioSet config={this.network_type} default={this.state.networkType} callback={this.updateState} />
+                <RadioSet config={this.osd_type} default={this.state.osdType} callback={this.updateState} />
+                <RadioSet config={this.flash_usage} default={this.state.flashUsage} callback={this.updateState} />
+                <RadioSet config={this.osd_mode} default={this.state.osdMode} callback={this.updateState} />
+                <RadioSet config={this.install_type} default={this.state.installType} callback={this.updateState} />
                 <div className="nav-button-container">
                     <UIButton primary btnLabel="Hosts &rsaquo;" action={this.checkReady} />
                 </div>
